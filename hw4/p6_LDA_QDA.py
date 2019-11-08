707ddc5f14384b98906d1d70e905f13fd4e46f54
Elaine <31362509+Loielaine@users.noreply.github.com>
2019-11-08 11:59:21 -0500

    Add files via upload

--- hw4/p6_LDA_QDA.py
+++ hw4/p6_LDA_QDA.py
@@ -34,15 +34,29 @@ QDA_score = np.diag(QDA(mu0, mu1, cov0, cov1, data.T))
 QDA_result = np.array(QDA_score>0)
 
 # LDA plot
-plt.scatter(x=data[:,0],y=data[:,1],c=LDA_result)
+w = (mu1-mu0).T.dot(np.linalg.inv(cov))
+b = -1/2*((mu1.T).dot(np.linalg.inv(cov)).dot(mu1)-(mu0.T).dot(np.linalg.inv(cov)).dot(mu0))[0][0]
+x = np.arange(0,6,0.1)
+y = (w[0,0]*x+b)/(-w[0,1])
+plt.scatter(x=data[:,0],y=data[:,1],c= LDA_result.reshape(-1))
+plt.plot(x,y)
 plt.xlabel('data[:,0]')
 plt.ylabel('data[:,1]')
 plt.title('LDA')
 plt.show()
 
 # QDA plot
-plt.scatter(x=data[:,0],y=data[:,1],c=QDA_result)
-plt.xlabel('data[:,0]')
-plt.ylabel('data[:,1]')
-plt.title('QDA')
-plt.show()
+from sympy import plot_implicit, cos, sin, symbols, Eq, And
+
+x, y = symbols('x y')
+X = np.array([x,y]).reshape((2,1))
+diff1 = np.linalg.inv(cov0) - np.linalg.inv(cov1)
+diff2 = np.linalg.inv(cov0).dot(mu0) -  np.linalg.inv(cov1).dot(mu1)
+threshold = 1/2*((mu1.T).dot(np.linalg.inv(cov)).dot(mu1)-(mu0.T).dot(np.linalg.inv(cov)).dot(mu0))[0][0] + \
+1/2*np.log(np.linalg.det(cov1)/np.linalg.det(cov0))
+expr = 1/2*(X.T.dot(diff1).dot(X)) - X.T.dot(diff2) - threshold
+
+plt2 = plot_implicit(Eq(expr[0,0],0),(x,-5,5), (y,-6,6))
+plt2._backend.ax.scatter(data_0[:,0],data_0[:,1],label='data_0')
+plt2._backend.ax.scatter(data_1[:,0],data_1[:,1],label='data_1')
+plt2._backend.save('plt2.png')
